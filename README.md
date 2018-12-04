3c29c504fbdb5c255a6a57fe02a4276a3581840d
Sam <samchamper@gmail.com>
2018-12-03 21:28:21 -0800

    Remove create statement from SQL DB.

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 ## SQL DATABASE FOR A FICTIONAL POKÉMON GAME AND APPLICATIONS THEREUPON
 
 
-## Author
-Samuel Champer
+# Author: Samuel Champer
 
 A repository for the final project for CIS 451 - Database Processing.
--- pokemon.sql
+++ pokemon.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `pokemon` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
-USE `pokemon`;
 -- MySQL dump 10.13  Distrib 8.0.12, for Win64 (x86_64)
 --
 -- Host: localhost    Database: pokemon
@@ -289,4 +287,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-12-03 19:07:12
+-- Dump completed on 2018-12-03 21:27:46
