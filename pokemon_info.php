8ab33434854ca30d8fd9122a9e50987d35e3a7a7
Sam <samchamper@gmail.com>
2018-12-04 14:06:28 -0800

    Iterate on applications.

--- pokemon_info.php
+++ pokemon_info.php
@@ -26,7 +26,7 @@ $pmon = mysqli_real_escape_string($conn, $pmon);
 // this is a small attempt to avoid SQL injection
 // better to use prepared statements
 
-$query = "p1.pokemon_id, p1.name, p1.type_1 AS type1, p1.type_2 AS type2, p1.hit_points, p1.attack, p1.defense, p1.special_attack, p1.special_defense, p1.speed, p1.legendary, p2.name AS evolves_to, p3.name AS second_evolution, p4.name AS previous_evolution, p5.name AS second_previous_evolution FROM pokemon p1 LEFT JOIN pokemon p2 on p1.pokemon_id=p2.evolves_from_id LEFT JOIN pokemon p3 on p2.pokemon_id=p3.evolves_from_id LEFT JOIN pokemon p4 on p4.pokemon_id=p1.evolves_from_id LEFT JOIN pokemon p5 on p5.pokemon_id=p4.evolves_from_id WHERE p1.name LIKE ";
+$query = "SELECT p1.pokemon_id, p1.name, p1.type_1 AS type1, p1.type_2 AS type2, p1.hit_points, p1.attack, p1.defense, p1.special_attack, p1.special_defense, p1.speed, p1.legendary, p2.name AS evolves_to, p3.name AS second_evolution, p4.name AS previous_evolution, p5.name AS second_previous_evolution FROM pokemon p1 LEFT JOIN pokemon p2 on p1.pokemon_id=p2.evolves_from_id LEFT JOIN pokemon p3 on p2.pokemon_id=p3.evolves_from_id LEFT JOIN pokemon p4 on p4.pokemon_id=p1.evolves_from_id LEFT JOIN pokemon p5 on p5.pokemon_id=p4.evolves_from_id WHERE p1.name LIKE ";
 $query = $query."'".$pmon."' OR p1.pokemon_id LIKE ";
 $query = $query."'".$pmon."';";
 
