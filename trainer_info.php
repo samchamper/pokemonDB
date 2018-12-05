a71629c3a51189a0be298cc6bdd126e9778e6939
Sam <samchamper@gmail.com>
2018-12-05 00:16:36 -0800

    Continue on applications.

--- trainer_info.php
+++ trainer_info.php
@@ -30,65 +30,32 @@ $pre_query = "SELECT loc_name FROM location WHERE loc_id LIKE '$traner_loc' OR l
 $query = "SELECT trainer_id FROM trainer WHERE route LIKE '$traner_loc';";
 ?>
 
-
+<p>
+The following initial querys was submitted to the pokemon database:
+<p>
+<?php
+print $pre_query;
+?>
+<p>
+<?php
+print $query;
+?>
+<p>
+Then the following queries used the return values from the above queries:
+<p>
+<?php
+print "SELECT name as pokemon_name, level FROM trainer JOIN trainer_has_pokemon ON trainer_num=trainer_id JOIN pokemon ON pokemon_num=pokemon_id WHERE trainer_id LIKE [RETURN VALUES FROM PREVIOUS QUERY];";
+?>
+<p>
+<?php
+print "WHERE trainer_id LIKE [RETURN VALUES FROM PREVIOUS QUERY];";
+?>
+<p>
 
 <hr>
 <p>
 
-<?php
-$result = mysqli_query($conn, $pre_query)
-or die(mysqli_error($conn));
 
-print "<pre>";
-if(! mysqli_num_rows($result))
-{
-	print "It looks like '$traner_loc' is an invalid location! Please try a different location!";
-}
-else
-{
-	$loc_name = "";
-	while($row = mysqli_fetch_array($result, MYSQLI_BOTH))
-	{
-		$loc_name = "$row[loc_name]";
-	}
-	$result = mysqli_query($conn, $query)
-	or die(mysqli_error($conn));
-
-	if(! mysqli_num_rows($result))
-	{
-		print "There are no no trainers in $loc_name! Please try a different location!";
-	}
-	else
-	{
-		print "The following trainers are present in $loc_name:";
-		while($row = mysqli_fetch_array($result, MYSQLI_BOTH))
-		{
-			print "\n";
-			print "Trainer $row[trainer_id]";
-		}
-		
-		print "\n\n"
-		$result = mysqli_query($conn, $query)
-		or die(mysqli_error($conn));
-		while($row = mysqli_fetch_array($result, MYSQLI_BOTH))
-		{
-			print "\n";
-			print "Trainer $row[trainer_id] has the following pokemon and items:\n";
-			
-			
-			
-			
-		}
-	}
-}
-?>
-
-
-<?php
-print "</pre>";
-mysqli_free_result($result);
-mysqli_close($conn);
-?>
 <p>
 <hr>
 </body>
