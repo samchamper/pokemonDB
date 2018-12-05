965d9a7847c95eeae456152db03e16875caed497
Sam <samchamper@gmail.com>
2018-12-05 01:46:58 -0800

    Continue to work on apps.

--- loc_info.php
+++ loc_info.php
@@ -25,7 +25,7 @@ $loc = mysqli_real_escape_string($conn, $loc);
 // this is a small attempt to avoid SQL injection
 // better to use prepared statements
 
-$query = "SELECT x.loc_id, x.loc_name, x.loc_type, COALESCE(n.loc_name, "nothing in particular") AS north, COALESCE(s.loc_name, "nothing in particular") AS south, COALESCE(w.loc_name, "nothing in particular") AS west, COALESCE(e.loc_name, "nothing in particular") AS east FROM location x LEFT JOIN location n on n.loc_id=x.north_loc LEFT JOIN location s on s.loc_id=x.south_loc LEFT JOIN location e on e.loc_id=x.east_loc LEFT JOIN location w on w.loc_id=x.west_loc WHERE x.loc_id LIKE '$loc' OR x.loc_name LIKE '$loc';";
+$query = "SELECT x.loc_id, x.loc_name, x.loc_type, COALESCE(n.loc_name, 'nothing in particular') AS north, COALESCE(s.loc_name, 'nothing in particular') AS south, COALESCE(w.loc_name, 'nothing in particular') AS west, COALESCE(e.loc_name, 'nothing in particular') AS east FROM location x LEFT JOIN location n on n.loc_id=x.north_loc LEFT JOIN location s on s.loc_id=x.south_loc LEFT JOIN location e on e.loc_id=x.east_loc LEFT JOIN location w on w.loc_id=x.west_loc WHERE x.loc_id LIKE '$loc' OR x.loc_name LIKE '$loc';";
 ?>
 
 <p>
@@ -62,51 +62,11 @@ else
 {
 	while($row = mysqli_fetch_array($result, MYSQLI_BOTH))
 	{
+		$loc_name = "$row[loc_name]";
 		print "Loc ID: $row[loc_name]\nLoc Name: $row[loc_name]\nLoc type: $row[loc_type].";
 		print "To the North lies $row[north], to the East lies $row[east]\n to the South lies $row[south] to the West lies $row[west].\n";
 	}
-	$result = mysqli_query($conn, $query)
-	or die(mysqli_error($conn));
 
-	if(! mysqli_num_rows($result))
-	{
-		print "There are no no trainers in $loc_name! Please try a different location!";
-	}
-	else
-	{
-		print "The following trainers are present in $loc_name:";
-		while($row = mysqli_fetch_array($result, MYSQLI_BOTH))
-		{
-			print "\n";
-			print " Trainer $row[trainer_id]";
-		}
-		
-		print "\n\n";
-		$result = mysqli_query($conn, $query)
-		or die(mysqli_error($conn));
-		while($row = mysqli_fetch_array($result, MYSQLI_BOTH))
-		{
-			$t_id = $row[trainer_id];
-			print "\n";
-			print "Trainer $t_id has the following pokemon and items:\n";
-			
-			$pokemon_query = "SELECT name, level FROM trainer JOIN trainer_has_pokemon ON trainer_num=trainer_id JOIN pokemon ON pokemon_num=pokemon_id WHERE trainer_id LIKE '$t_id';";
-			$subresult = mysqli_query($conn, $pokemon_query)
-			or die(mysqli_error($conn));
-			while($row = mysqli_fetch_array($subresult, MYSQLI_BOTH))
-			{
-				print  " A level $row[level] $row[name].\n";
-			}
-
-			$item_query = "SELECT identifier as item_name FROM trainer JOIN trainer_has_item ON trainer=trainer_id JOIN item ON item=item_id WHERE trainer_id LIKE '$t_id';";
-			$subresult2 = mysqli_query($conn, $item_query)
-			or die(mysqli_error($conn));
-			while($row = mysqli_fetch_array($subresult2, MYSQLI_BOTH))
-			{
-				print  " A $row[item_name].\n";
-			}
-		}
-	}
 }
 ?>
 
