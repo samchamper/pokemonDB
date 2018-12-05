3538f7b6d4b761f4904562a265b9c83bfd8fd7d6
Sam <samchamper@gmail.com>
2018-12-04 21:53:14 -0800

    Continue work on apps.

--- item_info.php
+++ item_info.php
@@ -30,7 +30,7 @@ $query = "SELECT item_id, identifier as item_name, short_effect as description F
 
 $query2 = "SELECT loc_name, number_in_stock, store_sells_for_price AS sale_price FROM item JOIN inventory i USING(item_id) JOIN town t on i.store_loc_id=t.town_loc_id JOIN location l on t.town_loc_id=l.loc_id WHERE number_in_stock > 0 AND (item_id LIKE '$item' OR identifier LIKE '$item');";
 
-$query3 = "SELECT loc_name, max(store_buys_for_price) AS store_buys_for FROM item JOIN inventory i USING(item_id) JOIN town t on i.store_loc_id=t.town_loc_id JOIN location l on t.town_loc_id=l.loc_id WHERE item_id LIKE '$item' OR identifier LIKE '$item';";
+$query3 = "SELECT loc_name, store_buys_for_price AS store_buys_for FROM item JOIN inventory i USING(item_id) JOIN town t on i.store_loc_id=t.town_loc_id JOIN location l on t.town_loc_id=l.loc_id JOIN (SELECT max(store_buys_for_price) max_price FROM inventory WHERE item_id LIKE '$item') t2 WHERE store_buys_for_price = max_price AND item_id LIKE '$item';";
 
 $query4 = "SELECT trainer_id, loc_name, x_coord, y_coord FROM item i JOIN trainer_has_item ti ON i.item_id=ti.item JOIN trainer t ON ti.trainer=t.trainer_id JOIN location ON t.route=location.loc_id WHERE item_id LIKE '$item' OR identifier LIKE '$item';";
 ?>
@@ -64,85 +64,66 @@ or die(mysqli_error($conn));
 print "<pre>";
 if(! mysqli_num_rows($result))
 {
-	$res_str = "There are no pokemon matching your query! ";
-	$res_str = $res_str."'".$pmon."' was not found in the database. Please try a different name/pokedex number!\n";
-    print $res_str;
+	print "There is no item matching your query! '$item' was not found in the database. Please try a different item name/number!";
 }
 else
 {
-while($row = mysqli_fetch_array($result, MYSQLI_BOTH))
-  {
-	$pmon_name = $row[name];
-    print "\n";
-	print "'".$pmon."' was found in the database, with the following information:\n\n";
-    print "Pokedex number: $row[pokemon_id]\nName: $row[name]\nType: $row[type1] $row[type2]\n";
-	print "Stats:\n Hit points $row[hit_points]\n Attack: $row[attack]\n Defense: $row[defense]\n Special Attack: $row[special_attack]\n Special Defense: $row[special_defense]\n Speed: $row[speed]\n\n";
-    if($row[evolves_to])
+	print "'$item' was found in the database, with the following information:\n\n";
+	while($row = mysqli_fetch_array($result, MYSQLI_BOTH))
 	{
-		print "$row[name] evolves into $row[evolves_to]";
-		if($row[second_evolution])
-		{
-			print " which in turn evolves into $row[second_evolution]";
-		}
-		print ".\n";
+		$item_name = $row[item_name];
+		print "\n";
+		print "Item id: $row[item_id]\nName: $row[item_name]\nDescription: $row[description]\n";
 	}
-	if($row[previous_evolution])
-	{
-		print "$row[name] evolves from $row[previous_evolution]";
-		if($row[second_previous_evolution])
-		{
-			print " which in turn evolves from $row[second_previous_evolution]";
-		}
-		print ".\n";
-	}
-	
-	if($row[legendary] == "TRUE")
-	{
-		print "$row[name] is a legendary pokemon. Approach with caution.\n";
-	}
-  }
 
-print "\n\n";
-$result = mysqli_query($conn, $query2)
-or die(mysqli_error($conn));
-
-if(! mysqli_num_rows($result))
-{
-	print "There are no routes along which $pmon_name can be captured!";
-}
-else
-{
-	print "$pmon_name can be captured on the following route(s):";
-}
-while($row = mysqli_fetch_array($result, MYSQLI_BOTH))
-  {
-    print "\n";
-    print " $row[loc_name]";
-  }
+	print "\n\n";
+	$result = mysqli_query($conn, $query2)
+	or die(mysqli_error($conn));	
+	if(! mysqli_num_rows($result))
+	{
+		print "There are no towns where $item_name is currently in stock!";
+	}
+	else
+	{
+		print "$item_name is currently in stock at the following location(s):\n";
+	}
+	while($row = mysqli_fetch_array($result, MYSQLI_BOTH))
+	{
+		print "\n";
+		print " $row[loc_name], where $row[number_in_stock] are available for $row[sale_price] each.";
+	}
 
   
-print "\n\n";
-$result = mysqli_query($conn, $query3)
-or die(mysqli_error($conn));
+	print "\n\n";
+	$result = mysqli_query($conn, $query3)
+	or die(mysqli_error($conn));
 
-if(! mysqli_num_rows($result))
-{
-	print "There are no trainers in possession of a $pmon_name!";
-}
-else
-{
-	print "The following trainers have a $pmon_name:";
-}
-while($row = mysqli_fetch_array($result, MYSQLI_BOTH))
-{
-    print "\n";
-    print " Trainer $row[trainer_id], located in $row[loc_name], has a level $row[level] $pmon_name";
-	if($row[loc_type] == "Town")
+	if(! mysqli_num_rows($result))
 	{
-		print ", and is a gym leader";
+		print "Error determining best place to sell item.";
 	}
-	print ".";
-}
+	while($row = mysqli_fetch_array($result, MYSQLI_BOTH))
+	{
+		print "The place that pays the highest price for $item_name is the shop in $row[loc_name], which will pay $row[store_buys_for] per $item_name. Consider selling there if you have extras!";
+	}
+
+	print "\n\n";
+	$result = mysqli_query($conn, $query4)
+	or die(mysqli_error($conn));	
+	if(! mysqli_num_rows($result))
+	{
+		print "There are no trainers who possess a $item_name!";
+	}
+	else
+	{
+		print "Currently, the following trainers are in possession of a $item_name:\n";
+	}
+	while($row = mysqli_fetch_array($result, MYSQLI_BOTH))
+	{
+		print "\n";
+		print " Trainer $row[trainer_id], located at $row[loc_name] at x coordinate $row[x_coord] and y coordinate $row[y_coord].";
+	}
+	
 }
 ?>
 
